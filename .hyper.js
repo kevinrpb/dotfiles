0d821756972edcaa934685beb26cf825f885558e
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2019-06-05 12:34:11 +0200

    Update .hyper.js

--- .hyper.js
+++ .hyper.js
@@ -3,202 +3,190 @@
 // See https://hyper.is#cfg for all currently supported options.
 
 module.exports = {
-  config: {
-    // choose either `'stable'` for receiving highly polished,
-    // or `'canary'` for less polished but more frequent updates
-    updateChannel: 'canary',
+	config: {
+		// choose either `'stable'` for receiving highly polished,
+		// or `'canary'` for less polished but more frequent updates
+		updateChannel: 'canary',
 
-    // default font size in pixels for all tabs
-    fontSize: 12,
+		// default font size in pixels for all tabs
+		fontSize: 12,
 
-    // font family with optional fallbacks
-    fontFamily: 'Fira Code, monospace',
+		// font family with optional fallbacks
+		fontFamily: 'Fira Code, monospace',
 
-    // default font weight: 'normal' or 'bold'
-    fontWeight: 'normal',
+		// default font weight: 'normal' or 'bold'
+		fontWeight: 'normal',
 
-    // font weight for bold characters: 'normal' or 'bold'
-    fontWeightBold: 'bold',
+		// font weight for bold characters: 'normal' or 'bold'
+		fontWeightBold: 'bold',
 
-    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: 'rgba(248, 28, 229, 0.8)',
+		// terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
+		cursorColor: 'rgba(248, 28, 229, 0.8)',
 
-    // terminal text color under BLOCK cursor
-    cursorAccentColor: '#000',
+		// terminal text color under BLOCK cursor
+		cursorAccentColor: '#000',
 
-    // `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
-    cursorShape: 'BEAM',
+		// `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
+		cursorShape: 'BEAM',
 
-    // set to `true` (without backticks and without quotes) for blinking cursor
-    cursorBlink: true,
+		// set to `true` (without backticks and without quotes) for blinking cursor
+		cursorBlink: true,
 
-    // color of the text
-    foregroundColor: 'rgb(191, 191, 191)',
+		// color of the text
+		foregroundColor: 'rgb(191, 191, 191)',
 
-    // terminal background color
-    // opacity is only supported on macOS
-    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+		// terminal background color
+		// opacity is only supported on macOS
+		backgroundColor: 'rgba(0, 0, 0, 0.6)',
 
-    // terminal selection color
-    cursorColor: 'rgba(248, 28, 229, 0.8)',
+		// terminal selection color
+		cursorColor: 'rgba(248, 28, 229, 0.8)',
 
-    // border color (window, tabs)
-    borderColor: 'rgba(0, 0, 0, 0.0)',
+		// border color (window, tabs)
+		borderColor: 'rgba(0, 0, 0, 0.0)',
 
-    // custom CSS to embed in the main window
-    css: ``,
+		// custom CSS to embed in the main window
+		css: ``,
 
-    // custom CSS to embed in the terminal window
-    termCSS: ``,
+		// custom CSS to embed in the terminal window
+		termCSS: ``,
 
-    // if you're using a Linux setup which show native menus, set to false
-    // default: `true` on Linux, `true` on Windows, ignored on macOS
-    showHamburgerMenu: '',
+		// if you're using a Linux setup which show native menus, set to false
+		// default: `true` on Linux, `true` on Windows, ignored on macOS
+		showHamburgerMenu: '',
 
-    // set to `false` (without backticks and without quotes) if you want to hide the minimize, maximize and close buttons
-    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
-    // default: `true` (without backticks and without quotes) on Windows and Linux, ignored on macOS
-    showWindowControls: '',
+		// set to `false` (without backticks and without quotes) if you want to hide the minimize, maximize and close buttons
+		// additionally, set to `'left'` if you want them on the left, like in Ubuntu
+		// default: `true` (without backticks and without quotes) on Windows and Linux, ignored on macOS
+		showWindowControls: '',
 
-    // custom padding (CSS format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
+		// custom padding (CSS format, i.e.: `top right bottom left`)
+		padding: '12px 14px',
 
-    // the full list. if you're going to provide the full color palette,
-    // including the 6 x 6 color cubes and the grayscale map, just provide
-    // an array here instead of a color map object
-    colors: {
-      black: '#000000',
-      red: '#C51E14',
-      green: '#1DC121',
-      yellow: '#C7C329',
-      blue: '#0A2FC4',
-      magenta: '#C839C5',
-      cyan: '#20C5C6',
-      white: '#C7C7C7',
-      lightBlack: '#686868',
-      lightRed: '#FD6F6B',
-      lightGreen: '#67F86F',
-      lightYellow: '#FFFA72',
-      lightBlue: '#6A76FB',
-      lightMagenta: '#FD7CFC',
-      lightCyan: '#68FDFE',
-      lightWhite: '#FFFFFF',
-    },
+		// the full list. if you're going to provide the full color palette,
+		// including the 6 x 6 color cubes and the grayscale map, just provide
+		// an array here instead of a color map object
+		colors: {
+			black: '#000000',
+			red: '#C51E14',
+			green: '#1DC121',
+			yellow: '#C7C329',
+			blue: '#0A2FC4',
+			magenta: '#C839C5',
+			cyan: '#20C5C6',
+			white: '#C7C7C7',
+			lightBlack: '#686868',
+			lightRed: '#FD6F6B',
+			lightGreen: '#67F86F',
+			lightYellow: '#FFFA72',
+			lightBlue: '#6A76FB',
+			lightMagenta: '#FD7CFC',
+			lightCyan: '#68FDFE',
+			lightWhite: '#FFFFFF',
+		},
 
-    hyperTransparent: {
-        backgroundColor: '#000',
-        //opacity: 0.8,
-        vibrancy: 'ultra-dark' // ['', 'dark', 'medium-light', 'ultra-dark']
-    },
+		hyperTransparent: {
+			backgroundColor: '#000',
+			//opacity: 0.8,
+			vibrancy: 'ultra-dark', // ['', 'dark', 'medium-light', 'ultra-dark']
+		},
 
-    hyperline: {
-      plugins: [
-        "hostname",
-        "ip",
-        "memory",
-        "cpu",
-        "network",
-        "spotify"
-      ]
-    },
+		hyperStatusLine: {
+			dirtyColor: 'salmon',
+			aheadColor: 'ivory',
+			footerTransparent: false,
+		},
 
-    hyperStatusLine: {
-        dirtyColor: 'salmon',
-        aheadColor: 'ivory',
-        footerTransparent: false
-    },
+		hyperTabs: {
+			trafficButtons: false,
+			// border: true,
+			// tabIcons: false,
+			tabIconsColored: true,
+			// activityColor: 'salmon',
+			// closeAlign: 'right',
+			// activityPulse: false,
+		},
 
-    hyperTabs: {
-        trafficButtons: false,
-        // border: true,
-        // tabIcons: false,
-        tabIconsColored: true,
-        // activityColor: 'salmon',
-        // closeAlign: 'right',
-        // activityPulse: false,
-    },
-    
-    hypercwd: {
-        initialWorkingDirectory: '~/Documents'
-    },
-    
-    hyperPostprocessing: {
-        // defaults to `${HOME}/.hyper-postprocessing.js`
-        entry: '${HOME}/.hyper-postprocessing.js'
-    },
+		hypercwd: {
+			initialWorkingDirectory: '~/Documents',
+		},
 
-    commands: ['clear'],
+		hyperPostprocessing: {
+			// defaults to `${HOME}/.hyper-postprocessing.js`
+			entry: '${HOME}/.hyper-postprocessing.js',
+		},
 
-    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-    // if left empty, your system's login shell will be used by default
-    //
-    // Windows
-    // - Make sure to use a full path if the binary name doesn't work
-    // - Remove `--login` in shellArgs
-    //
-    // Bash on Windows
-    // - Example: `C:\\Windows\\System32\\bash.exe`
-    //
-    // PowerShell on Windows
-    // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
-    shell: '/bin/zsh',
+		commands: ['clear'],
 
-    // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
-    // by default `['--login']` will be used
-    shellArgs: ['--login'],
+		// the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
+		// if left empty, your system's login shell will be used by default
+		//
+		// Windows
+		// - Make sure to use a full path if the binary name doesn't work
+		// - Remove `--login` in shellArgs
+		//
+		// Bash on Windows
+		// - Example: `C:\\Windows\\System32\\bash.exe`
+		//
+		// PowerShell on Windows
+		// - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
+		shell: '/bin/zsh',
 
-    // for environment variables
-    env: {},
+		// for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
+		// by default `['--login']` will be used
+		shellArgs: ['--login'],
 
-    // set to `false` for no bell
-    bell: 'SOUND',
+		// for environment variables
+		env: {},
 
-    // if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
-    copyOnSelect: false,
+		// set to `false` for no bell
+		bell: 'SOUND',
 
-    // if `true` (without backticks and without quotes), hyper will be set as the default protocol client for SSH
-    defaultSSHApp: true,
+		// if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
+		copyOnSelect: false,
 
-    // if `true` (without backticks and without quotes), on right click selected text will be copied or pasted if no
-    // selection is present (`true` by default on Windows and disables the context menu feature)
-    // quickEdit: true,
+		// if `true` (without backticks and without quotes), hyper will be set as the default protocol client for SSH
+		defaultSSHApp: true,
 
-    // URL to custom bell
-    // bellSoundURL: 'http://example.com/bell.mp3',
+		// if `true` (without backticks and without quotes), on right click selected text will be copied or pasted if no
+		// selection is present (`true` by default on Windows and disables the context menu feature)
+		// quickEdit: true,
 
-    // for advanced config flags please refer to https://hyper.is/#cfg
-  },
+		// URL to custom bell
+		// bellSoundURL: 'http://example.com/bell.mp3',
 
-  // a list of plugins to fetch and install from npm
-  // format: [@org/]project[#version]
-  // examples:
-  //   `hyperpower`
-  //   `@company/project`
-  //   `project#1.0.1`
-  plugins: [
-    "hyper-sync-settings",
-    "hyper-search",
-    "hypercwd",
-    "hyper-color-command",
-    "hyper-startup",
-    "hyper-alt-click",
-    "hyperterm-safepaste",
-    "hyper-match",
-    //"hyperline",
-    "hyper-statusline",
-    "hyper-tabs-enhanced",
-    "hyperminimal",
-    "hyper-transparent",
-    "hyper-postprocessing"
-  ],
+		// for advanced config flags please refer to https://hyper.is/#cfg
+	},
 
-  // in development, you can create a directory under
-  // `~/.hyper_plugins/local/` and include it here
-  // to load it and avoid it being `npm install`ed
-  localPlugins: [],
+	// a list of plugins to fetch and install from npm
+	// format: [@org/]project[#version]
+	// examples:
+	//   `hyperpower`
+	//   `@company/project`
+	//   `project#1.0.1`
+	plugins: [
+		'hyper-sync-settings',
+		'hyper-search',
+		'hypercwd',
+		'hyper-color-command',
+		'hyper-startup',
+		'hyper-alt-click',
+		'hyperterm-safepaste',
+		'hyper-match',
+		// 'hyper-statusline',
+		'hyper-tabs-enhanced',
+		'hyperminimal',
+		'hyper-transparent',
+		'hyper-postprocessing',
+	],
 
-  keymaps: {
-    // Example
-    // 'window:devtools': 'cmd+alt+o',
-  },
+	// in development, you can create a directory under
+	// `~/.hyper_plugins/local/` and include it here
+	// to load it and avoid it being `npm install`ed
+	localPlugins: [],
+
+	keymaps: {
+		// Example
+		// 'window:devtools': 'cmd+alt+o',
+	},
 };
